c2e2ee703d1914ad8dea9f946aa56773cc6f9c9a
Eugenio-Grant-AWS <eugenio.grant+aws@gmail.com>
2025-01-13 13:33:03 -0300

    new changes

--- routes/auth.php
+++ routes/auth.php
@@ -44,7 +44,7 @@ Route::middleware('guest')->group(function () {
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
         ->name('password.reset');
     Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.update');
+        ->name('password.reset.update');
 
     // Resend OTP Route
     Route::post('forgot-password/resendOtp', [PasswordResetLinkController::class, 'resendOtp'])
