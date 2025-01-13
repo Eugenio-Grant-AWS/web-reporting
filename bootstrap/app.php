2b574327f6e3340dd36bd28bc3267d8e67e16f70
Eugenio-Grant-AWS <eugenio.grant+aws@gmail.com>
2025-01-13 13:20:53 -0300

    new changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'track.previous.url' => TrackPreviousUrl::class,
-            'check.route' => \App\Http\Middleware\CheckRoute::class,
+            // 'check.route' => \App\Http\Middleware\CheckRoute::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
