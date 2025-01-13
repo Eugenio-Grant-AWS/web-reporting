f7fdeaf5b58038cf9f903a4ca7790407d9bb2e70
Eugenio-Grant-AWS <eugenio.grant+aws@gmail.com>
2025-01-13 13:43:51 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
 // })->name('dashboard');
 
 
-Route::middleware(['auth', 'verified', 'track.previous.url'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
